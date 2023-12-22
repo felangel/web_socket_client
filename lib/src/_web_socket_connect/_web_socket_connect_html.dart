3436cd996b5a4a1539ef97f8962789e63dfcab7a
mytja <mytja@protonmail.com>
2023-12-22 19:14:47 +0100

    migrate to pkg:web

--- lib/src/_web_socket_connect/_web_socket_connect_html.dart
+++ lib/src/_web_socket_connect/_web_socket_connect_html.dart
@@ -1,5 +1,19 @@
 import 'dart:async';
-import 'dart:html';
+import 'dart:js_interop';
+
+import 'package:web/helpers.dart';
+
+// TODO: remove when https://github.com/dart-lang/web/commit/4cb5811ed06
+// is in a published release and the min constraint on pkg:web is updated
+extension WebSocketEvents on WebSocket {
+  Stream<Event> get onOpen => EventStreamProviders.openEvent.forTarget(this);
+  Stream<MessageEvent> get onMessage =>
+      EventStreamProviders.messageEvent.forTarget(this);
+  Stream<CloseEvent> get onClose =>
+      EventStreamProviders.closeEvent.forTarget(this);
+  Stream<Event> get onError =>
+      EventStreamProviders.errorEventSourceEvent.forTarget(this);
+}
 
 /// Create a WebSocket connection.
 Future<WebSocket> connect(
@@ -8,7 +22,10 @@ Future<WebSocket> connect(
   Duration? pingInterval,
   String? binaryType,
 }) async {
-  final socket = WebSocket(url, protocols)..binaryType = binaryType;
+  final socket = WebSocket(
+    url,
+    protocols?.map((e) => e.toJS).toList().toJS ?? JSArray(),
+  )..binaryType = binaryType ?? 'list';
 
   if (socket.readyState == 1) return socket;
 
