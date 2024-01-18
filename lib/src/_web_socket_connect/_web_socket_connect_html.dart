3711167131c4b4dc2b858f98850dd712bba89a77
Mitja Ševerkar <mitja@severkar.eu>
2024-01-18 07:52:08 +0100

    feat: migrate to `package:web` (#33)

--- lib/src/_web_socket_connect/_web_socket_connect_html.dart
+++ lib/src/_web_socket_connect/_web_socket_connect_html.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
-import 'dart:html';
+import 'dart:js_interop';
+
+import 'package:web/helpers.dart';
 
 /// Create a WebSocket connection.
 Future<WebSocket> connect(
@@ -8,7 +10,13 @@ Future<WebSocket> connect(
   Duration? pingInterval,
   String? binaryType,
 }) async {
-  final socket = WebSocket(url, protocols)..binaryType = binaryType;
+  final socket = WebSocket(
+    url,
+    protocols?.map((e) => e.toJS).toList().toJS ?? JSArray(),
+  )
+    // Either "blob" (default) or "arraybuffer".
+    // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/binaryType
+    ..binaryType = binaryType ?? 'blob';
 
   if (socket.readyState == 1) return socket;
 
