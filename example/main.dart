a85e1e3ba7c29c0cd2b95c62b25f80de03371ce4
XanderD99 <xander.denecker@be-mobile.com>
2024-03-11 16:13:55 +0100

    fix example

--- example/main.dart
+++ example/main.dart
@@ -7,8 +7,6 @@ void main() async {
   const backoff = ConstantBackoff(Duration(seconds: 1));
   final socket = WebSocket(uri, backoff: backoff);
 
-  await socket.connect();
-
   // Listen for changes in the connection state.
   socket.connection.listen((state) => print('state: "$state"'));
 
